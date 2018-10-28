9799122e6324250dff0b98ebfecb8e294f5a14cb
bdzanko17 <bdzanko1@gmail.com>
2018-10-28 17:17:52 +0100

    Abstraktna klasa dodana

---
+++ src/ba/unsa/etf/rpr/ChessPiece.java
@@ -0,0 +1,18 @@
+package ba.unsa.etf.rpr;
+
+public abstract class ChessPiece {
+    public static enum Color {
+        BLACK ,WHITE
+    };
+    private String pozicija;
+    private Color boja;
+    public abstract String getPosition();
+    public abstract Color getColor();
+    public abstract void move(String position)throws IllegalArgumentException;
+    ChessPiece(String pozicija,Color boja){
+    }
+
+
+
+
+}
