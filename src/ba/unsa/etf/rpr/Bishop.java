8fb64960096819c84886dc4a3b34cd43d7e0ea4c
bdzanko17 <bdzanko1@gmail.com>
2018-10-30 20:15:38 +0100

    Lovac završen

---
+++ src/ba/unsa/etf/rpr/Bishop.java
@@ -0,0 +1,52 @@
+package ba.unsa.etf.rpr;
+import static java.lang.Math.abs;
+
+public class Bishop extends ChessPiece {
+    Bishop(String pozicija, Color boja) throws IllegalArgumentException {
+        super(pozicija, boja);
+    }
+    public static boolean legalan_za_lovca(char slovo, char broj, char slovo2, char broj2) {
+        boolean x = false;
+        if(slovo!=slovo2&&broj!=broj2&&(abs(slovo2-slovo)==abs(broj2-broj))) x=true;
+        return x;
+    }
+    @Override
+    public void move(String position) throws IllegalArgumentException, IllegalChessMoveException {
+        if (position.length() > 2) throw new IllegalArgumentException(); //provjera da li je dobra pozicija
+
+        String s = this.getPozicija();
+        System.out.println(s);
+        char slovo = s.charAt(0);
+        char broj = s.charAt(1);
+        char slovo2 = position.charAt(0);
+        char broj2 = position.charAt(1);
+        if ((position.charAt(0) >= 'A' && position.charAt(0) <= 'H') && position.charAt(1) >= '1' && position.charAt(1) <= '8') {
+            if (legalan_za_lovca(slovo, broj, slovo2, broj2)) {
+                this.setPozicija(position);
+            } else {
+                throw new IllegalChessMoveException("Nemoguc potez");  //izuzetak
+            }
+        } else throw new IllegalArgumentException();
+
+    }
+
+    @Override
+    public void setPozicija(String pozicija) {
+        this.pozicija=pozicija;
+    }
+
+    @Override
+    public void setBoja(Color boja) {
+        this.boja=boja;
+    }
+
+    @Override
+    public String  getPozicija() {
+        return this.pozicija;
+    }
+
+    @Override
+    public Color getBoja() {
+        return this.boja;
+    }
+}
--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -2,8 +2,8 @@ package ba.unsa.etf.rpr;
 
 public class Main {
      public static void main(String[] args) throws ChessPiece.IllegalChessMoveException {
-        ChessPiece kralj=new King("A1",ChessPiece.Color.WHITE);
-        kralj.move("A22222");
-        System.out.println(((King) kralj).getPozicija());
+        ChessPiece kralj=new Bishop("A1",ChessPiece.Color.WHITE);
+        kralj.move("B2");
+        System.out.println(kralj.getPozicija());
      }
 }
