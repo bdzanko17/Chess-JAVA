2d34195f55e7c474f4a1c2d67739cdb4392e44db
bdzanko17 <bdzanko1@gmail.com>
2018-10-30 21:04:38 +0100

    Dodan piun klasa

--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -2,8 +2,8 @@ package ba.unsa.etf.rpr;
 
 public class Main {
      public static void main(String[] args) throws ChessPiece.IllegalChessMoveException {
-        ChessPiece kralj=new Queen("E4",ChessPiece.Color.WHITE);
-        kralj.move("A6");
+        ChessPiece kralj=new Pawn("A2",ChessPiece.Color.WHITE);
+        kralj.move("A4");
         System.out.println(kralj.getPozicija());
      }
 }
--- src/ba/unsa/etf/rpr/Pawn.java
+++ src/ba/unsa/etf/rpr/Pawn.java
@@ -2,19 +2,14 @@ package ba.unsa.etf.rpr;
 
 import static java.lang.Math.abs;
 
-public class Queen extends ChessPiece {
-
-
-    public static boolean legalan_za_kraljiicu(char slovo, char broj, char slovo2, char broj2) {
-        boolean x = false;
-        if((abs(slovo2-slovo)==abs(broj2-broj)) || (slovo==slovo2 &&broj!=broj2)||(slovo!=slovo2 &&broj==broj2)    )
-            x=true;
-
-        return x;
-    }
-
-    Queen(String pozicija, Color boja) throws IllegalArgumentException {
+public class Pawn extends ChessPiece {
+    Pawn(String pozicija, Color boja) throws IllegalArgumentException {
         super(pozicija, boja);
+    }
+    public static boolean legalan_za_piuna(char slovo, char broj, char slovo2, char broj2) {
+        boolean x = false;
+        if((slovo==slovo2 &&broj!=broj2&&broj2==broj+1 )||(slovo+1==slovo2&&broj2==broj+1))x=true;
+        return x;
     }
 
     @Override
@@ -28,24 +23,23 @@ public class Queen extends ChessPiece {
         char slovo2 = position.charAt(0);
         char broj2 = position.charAt(1);
         if ((position.charAt(0) >= 'A' && position.charAt(0) <= 'H') && position.charAt(1) >= '1' && position.charAt(1) <= '8') {
-            if (legalan_za_kraljiicu(slovo, broj, slovo2, broj2)) {
+            if (legalan_za_piuna(slovo, broj, slovo2, broj2)) {
                 this.setPozicija(position);
             } else {
                 throw new IllegalChessMoveException("Nemoguc potez");  //izuzetak
             }
         } else throw new IllegalArgumentException();
-
-
     }
+
 
     @Override
     public void setPozicija(String pozicija) {
-            this.pozicija=pozicija;
+        this.pozicija=pozicija;
     }
 
     @Override
     public void setBoja(Color boja) {
-            this.boja=boja;
+        this.boja=boja;
     }
 
     @Override
@@ -55,6 +49,6 @@ public class Queen extends ChessPiece {
 
     @Override
     public Color getBoja() {
-            return this.boja;
+        return this.boja;
     }
 }
