d9567a1d59a7fe39b88275120c2be0d0c39715fb
bdzanko17 <bdzanko1@gmail.com>
2018-10-30 18:41:09 +0100

    metoda move

--- src/ba/unsa/etf/rpr/ChessPiece.java
+++ src/ba/unsa/etf/rpr/ChessPiece.java
@@ -4,21 +4,20 @@ public abstract class ChessPiece {
     public static enum Color {
         BLACK ,WHITE
     };
-    private String pozicija;
-    private Color boja;
-    public abstract String getPosition();
-    public abstract Color getColor();
+    protected String pozicija;
+    protected Color boja;
     public abstract void move(String position)throws IllegalArgumentException;
+    public abstract void setPozicija(String pozicija) ;
+    public abstract void setBoja(Color boja);
 
     ChessPiece(String pozicija,Color boja)throws IllegalArgumentException{
         if(pozicija.length()>2)throw new IllegalArgumentException();
-        if((pozicija.charAt(0)>='A'||pozicija.charAt(0)<='H')&&pozicija.charAt(1)>='1'&&pozicija.charAt(1)<='8'){
+        if((pozicija.charAt(0)>='A'&&pozicija.charAt(0)<='H')&&pozicija.charAt(1)>='1'&&pozicija.charAt(1)<='8'){
             this.pozicija=pozicija;
             this.boja=boja;
         }else throw new IllegalArgumentException();
 
     }
-
 
 
 
--- src/ba/unsa/etf/rpr/King.java
+++ src/ba/unsa/etf/rpr/King.java
@@ -1,25 +1,53 @@
 package ba.unsa.etf.rpr;
 
+import static java.lang.Math.abs;
+
 public class King extends ChessPiece {
 
     King(String pozicija, Color boja) {
-        super(pozicija, boja);
-
+    super(pozicija,boja);
+    }
+    public static boolean legalan_za_kralja(char slovo,char broj,char slovo2,char broj2){
+        System.out.println(slovo+broj+slovo2+broj2);
+        boolean x=false;
+        if(abs(slovo-slovo2)<=1){
+            if(abs(broj2-broj)<=1){
+                x=true;
+            }
+        }
+        return true;
     }
 
-    @Override
-    public String getPosition() {
-        return null;
-    }
 
-    @Override
-    public Color getColor() {
-        return null;
-    }
+
 
     @Override
     public void move(String position) throws IllegalArgumentException {
-        if((position.charAt(0)>='A'||position.charAt(0)<='H')&&position.charAt(1)>='1'&&position.charAt(1)<='8'){
-
+        String s=this.getPosition();
+        System.out.println(s);
+        char slovo=s.charAt(0);
+        char broj=s.charAt(1);
+        char slovo2=position.charAt(0);
+        char broj2=position.charAt(1);
+        if ((position.charAt(0) >= 'A' && position.charAt(0) <= 'H') && position.charAt(1) >= '1' && position.charAt(1) <= '8') {
+                    if(legalan_za_kralja(slovo,broj,slovo2,broj2)){
+                        this.setPozicija(position);
+                    }
         }
+    }
+    @Override
+    public void setBoja(Color boja) {
+        this.boja=boja;
+    }
+    @Override
+    public void setPozicija(String pozicija) {
+        this.pozicija=pozicija;
+            }
+    public String getPosition() {
+        return this.pozicija;
+    }
+
+    public Color getColor() {
+        return this.boja;
+    }
 }
