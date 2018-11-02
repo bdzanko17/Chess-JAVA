cef48c89a1377da91414eeff8c8d3b2a8ac457fb
bdzanko17 <bdzanko1@gmail.com>
2018-11-02 18:02:01 +0100

    vvs

--- src/ba/unsa/etf/rpr/Board.java
+++ src/ba/unsa/etf/rpr/Board.java
@@ -10,8 +10,8 @@ public class Board {
 
 
     Board() {
-        tabla = new ChessPiece[8][8];
 
+        tabla = new ChessPiece[8][8];
         for (int i = 0; i < 8; i++) {               //Postavljanje pijuna bijelih i crnih
             char x = (char) ('A' + i);
             String pozicija1 = x + Integer.toString(7);
@@ -20,41 +20,43 @@ public class Board {
             tabla[1][i] = new Pawn(pozicija2, ChessPiece.Color.WHITE);
 
         }
-        for(int i=0;i<8;i++){
-            for(int j=0;j<8;j++){
-                if(i!= 6 && i!=1 && i!=0 && i!=7){
-                    tabla[i][j]=null;
+
+        //Prazna polja
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (i != 6 && i != 1 && i != 0 && i != 7) {
+                    tabla[i][j] = null;
                 }
             }
         }
         //crne
 
-        tabla[0][0]=new Rook("A8", ChessPiece.Color.BLACK);
-        tabla[0][1]=new Knight("B8", ChessPiece.Color.BLACK);
-        tabla[0][2]=new Bishop("C8", ChessPiece.Color.BLACK);
-        tabla[0][3]=new Queen("D8", ChessPiece.Color.BLACK);
-        tabla[0][4]=new King("E8", ChessPiece.Color.BLACK);
-        tabla[0][5]=new Bishop("F8", ChessPiece.Color.BLACK);
-        tabla[0][6]=new Knight("G8", ChessPiece.Color.BLACK);
-        tabla[0][7]=new Rook("H8", ChessPiece.Color.BLACK);
+        tabla[0][0] = new Rook("A8", ChessPiece.Color.BLACK);
+        tabla[0][1] = new Knight("B8", ChessPiece.Color.BLACK);
+        tabla[0][2] = new Bishop("C8", ChessPiece.Color.BLACK);
+        tabla[0][3] = new Queen("D8", ChessPiece.Color.BLACK);
+        tabla[0][4] = new King("E8", ChessPiece.Color.BLACK);
+        tabla[0][5] = new Bishop("F8", ChessPiece.Color.BLACK);
+        tabla[0][6] = new Knight("G8", ChessPiece.Color.BLACK);
+        tabla[0][7] = new Rook("H8", ChessPiece.Color.BLACK);
 
         //bijele
 
-        tabla[7][0]=new Rook("A1", ChessPiece.Color.WHITE);
-        tabla[7][1]=new Knight("B1", ChessPiece.Color.WHITE);
-        tabla[7][2]=new Bishop("C1", ChessPiece.Color.WHITE);
-        tabla[7][3]=new Queen("D1", ChessPiece.Color.WHITE);
-        tabla[7][4]=new King("E1", ChessPiece.Color.WHITE);
-        tabla[7][5]=new Bishop("F1", ChessPiece.Color.WHITE);
-        tabla[7][6]=new Knight("G1", ChessPiece.Color.WHITE);
-        tabla[7][7]=new Rook("H1", ChessPiece.Color.WHITE);
+        tabla[7][0] = new Rook("A1", ChessPiece.Color.WHITE);
+        tabla[7][1] = new Knight("B1", ChessPiece.Color.WHITE);
+        tabla[7][2] = new Bishop("C1", ChessPiece.Color.WHITE);
+        tabla[7][3] = new Queen("D1", ChessPiece.Color.WHITE);
+        tabla[7][4] = new King("E1", ChessPiece.Color.WHITE);
+        tabla[7][5] = new Bishop("F1", ChessPiece.Color.WHITE);
+        tabla[7][6] = new Knight("G1", ChessPiece.Color.WHITE);
+        tabla[7][7] = new Rook("H1", ChessPiece.Color.WHITE);
 
-        for(int i=0;i<8;i++){
-            for(int j=0;j<8;j++){
-                if(!(i!= 6 && i!=1 && i!=0 && i!=7)){
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (tabla[i][j] != null)
                     System.out.println(tabla[i][j].getClass());
-                }            }
+            }
         }
-
     }
+
 }
