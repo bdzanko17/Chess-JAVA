c742b0f7a17995bdac6a93e6c70f1bf4cbc22fb6
bdzanko17 <bdzanko1@gmail.com>
2018-11-08 01:42:35 +0100

    otkrivena greska u pawn i rook ne bacaju izuzetak

--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -5,17 +5,11 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) throws IllegalChessMoveException, CloneNotSupportedException {
         Board b = new Board();
-        try {
-            b.move(Pawn.class, ChessPiece.Color.WHITE, "E4");
-            b.move(Pawn.class, ChessPiece.Color.WHITE, "E5");
-            b.move(Pawn.class, ChessPiece.Color.WHITE, "E6");
-            b.move(Pawn.class, ChessPiece.Color.WHITE, "D7");
-            b.move(Pawn.class, ChessPiece.Color.WHITE, "C8");
-            b.move(Queen.class, ChessPiece.Color.WHITE, "E2");
-        } catch(Exception e) {
-            // Do nothing
+        try{
+b.move(Rook.class, ChessPiece.Color.WHITE,"E5");
+        }catch (Exception e){
+            System.out.println("izuzetak");
         }
-        System.out.println(b.isCheck(ChessPiece.Color.BLACK));
 }
         }
 
