808ac7b3f585ca121462fd83ac4e46f7c0eab562
bdzanko17 <bdzanko1@gmail.com>
2018-11-09 22:11:06 +0100

    jos imam jedan problem a to je šta ako slijedeci pijun moze da se pomjeri za 2 mjesta a ovaj prethodni se pomjerio za jedno pa onda ukoso mu ne da i odma baci izuzetak

--- src/ba/unsa/etf/rpr/Pawn.java
+++ src/ba/unsa/etf/rpr/Pawn.java
@@ -6,9 +6,11 @@ public class Pawn extends ChessPiece {
     Pawn(String pozicija, Color boja) throws IllegalArgumentException {
         super(pozicija, boja);
     }
+    public Boolean diran=false;
 
     private static boolean legalan_za_piuna(char slovo, char broj, char slovo2, char broj2, Color boja) {
         boolean x = false;
+
 
         if (boja == Color.WHITE) {
             if ((slovo == slovo2 && abs(broj2 - broj) == 1 && broj2 > broj) || (abs(slovo - slovo2) == 1 && broj2 > broj && abs(broj - broj2) == 1)
@@ -36,17 +38,29 @@ public class Pawn extends ChessPiece {
         if (position.length() > 2 || position.length() <= 1)
             throw new IllegalArgumentException(); //provjera da li je dobra pozicija
 
-
         if (((position.charAt(0) >= 'A' && position.charAt(0) <= 'H' || (position.charAt(0) >= 'a' && position.charAt(0) <= 'h'))
                 && position.charAt(1) >= '1' && position.charAt(1) <= '8')) {
             String s = this.getPozicija();
             s.toUpperCase();
+            if(!(this.pozicija.equals("A2")||this.pozicija.equals("B2")||this.pozicija.equals("A2")
+                    ||this.pozicija.equals("C2")||this.pozicija.equals("D2")||this.pozicija.equals("E2")
+                    ||this.pozicija.equals("F2")||this.pozicija.equals("G2")||this.pozicija.equals("H2")
+                    ||this.pozicija.equals("A7")||this.pozicija.equals("B7")||this.pozicija.equals("C7")
+                    ||this.pozicija.equals("C7")||this.pozicija.equals("D7")||this.pozicija.equals("E7")
+                    ||this.pozicija.equals("F7")||this.pozicija.equals("G7")||this.pozicija.equals("H7")
+            )&&diran==true)
+            {
+             if(abs(position.charAt(1)-s.charAt(1))>1){
+                 throw new IllegalChessMoveException("greska");
+             }
+            }
             char slovo = s.charAt(0);
             char broj = s.charAt(1);
             char slovo2 = position.charAt(0);
             char broj2 = position.charAt(1);
             if (legalan_za_piuna(slovo, broj, slovo2, broj2, this.getBoja())) {
                 this.setPozicija(position);
+                diran=true;
             } else {
                 throw new IllegalChessMoveException("Nemoguc potez");  //izuzetak
             }
