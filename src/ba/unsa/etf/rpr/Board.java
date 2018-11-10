54ceb11dcb9af3b9317da3aa0a79e6ce0888aa91
bdzanko17 <bdzanko1@gmail.com>
2018-11-10 10:53:15 +0100

    moram vidjeti da li moze slijedeca figura da prodje

--- src/ba/unsa/etf/rpr/Board.java
+++ src/ba/unsa/etf/rpr/Board.java
@@ -217,8 +217,7 @@ public class Board {
                     } else {
                         if (type == Pawn.class) {
                             if (!DaliPijunJedePravo(position, tabla[i].getPozicija())) {
-                                System.out.println(tabla[i].getPozicija());
-                                    System.out.println("KAKO OVO PRODJEEEEEEEE");
+                                    ChessPiece next=
                                 if (!daliImaNaOdredistuFigure(tabla, position)) {
                                     throw new IllegalChessMoveException("ne moze ici ukoso bezveze");
                                 }
